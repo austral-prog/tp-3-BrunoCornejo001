caa62820a21eb417b8b3e6e3c6a0d08a3ee1c932
BrunoCornejo001 <bcornejo@mail.austral.edu.ar>
2025-06-27 23:52:44 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,11 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
+    texto = "Awesome".lower()  # Aseguramos todo en minúsculas
 
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+    primeras_tres = texto[:3]
+    medio = texto[2:5]  # Posiciones 2, 3 y 4 (centrales)
+    primera_a_cuarta = texto[:4]
+    antepenultima_a_ultima = texto[-3:]
+
+    print(primeras_tres)
+    print(medio)
+    print(primera_a_cuarta + antepenultima_a_ultima)
