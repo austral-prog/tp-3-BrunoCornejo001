50dcd8f7bbf3130935eb33cdc3cedcb665a0cd41
BrunoCornejo001 <bcornejo@mail.austral.edu.ar>
2025-06-28 00:03:10 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,4 +1,4 @@
 def slice_simple():
     texto = input("Ingrese un texto: ")
     resultado = texto[4::2]  # Desde el 5º carácter (índice 4) en adelante, saltando de a 2
-    print(resultado)
+print(resultado)
