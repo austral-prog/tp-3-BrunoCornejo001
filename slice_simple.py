1c90e12d0ba7aa9848529706bf52d39a14f29d85
BrunoCornejo001 <bcornejo@mail.austral.edu.ar>
2025-06-29 20:15:32 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,3 +1,10 @@
-def slice_advanced():
-    texto = input()
-    print(texto[4::2])
+texto = "Awesome".lower()
+
+primeras_3 = texto[:3]
+tres_del_medio = texto[2:5]  # asumiendo que siempre será el mismo texto "awesome"
+primera_a_cuarta = texto[:4]
+antepenultima_a_ultima = texto[-3:]
+
+print(primeras_3)
+print(tres_del_medio)
+print(primera_a_cuarta + antepenultima_a_ultima)
