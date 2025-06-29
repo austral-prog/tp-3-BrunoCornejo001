6673231989dfbbab8e43c98f2d8a9ae8a044d042
BrunoCornejo001 <bcornejo@mail.austral.edu.ar>
2025-06-29 20:10:48 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,4 +1,5 @@
-def slice_advanced():  # Aseguramos todo en minúsculas
-    texto = input("Ingrese un texto: ")
-    resultado = texto[4::2]  # Desde el 5º carácter (índice 4) en adelante, saltando de a 2
-print(resultado)
+def slice_advanced():
+    texto = input()
+    resultado = texto[4::2]
+    print(resultado)
+
