50ef319cd6a5be44209562759c238f252b3aae57
BrunoCornejo001 <bcornejo@mail.austral.edu.ar>
2025-06-29 20:19:39 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -4,7 +4,6 @@ def slice_simple():
   tres_del_medio = texto[2:5]  # asumiendo que siempre será el mismo texto "awesome"
   primera_a_cuarta = texto[:4]
   antepenultima_a_ultima = texto[-3:]
-
-print(primeras_3)
-print(tres_del_medio)
-print(primera_a_cuarta + antepenultima_a_ultima)
+  print(primeras_3)
+  print(tres_del_medio)
+  print(primera_a_cuarta + antepenultima_a_ultima)
